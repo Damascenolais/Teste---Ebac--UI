cca3144e90acf787e9ead947300d714886bbdbdf
lais.damasceno <lais.damascenoal@gmail.comgit config --global user.email lais.damascenoal@gmail.com>
2024-03-30 16:28:01 -0300

    testes negativos e hooks

--- cypress/e2e/3 -loja-ebac/login.cy.js
+++ cypress/e2e/3 -loja-ebac/login.cy.js
@@ -1,8 +1,17 @@
 ///<reference types="cypress"/>
 
 describe ('funcionalidade: login', () => {
+
+beforeEach(() => {
+    cy.visit ('http://lojaebac.ebaconline.art.br/my-account/')
+});
+
+
+afterEach(() => {
+    cy.screenshot()
+});
+    
     it ('deve fazer login com sucesso' , () => {
-        cy.visit ('http://lojaebac.ebaconline.art.br/my-account/')
         cy.get('#username').type ('lais.teste@teste.com.br')
         cy.get('#password').type ('teste@123')
         cy.get('.woocommerce-form > .button').click()
@@ -11,4 +20,21 @@ describe ('funcionalidade: login', () => {
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lais.teste (não é lais.teste? Sair)')
     })
 
+    it('Deve exibir uma mensagem de erro ai inserir usuário inválido', () => {
+        cy.get('#username').type ('lais@teste.com.br')
+        cy.get('#password').type ('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should ('contain', 'Endereço de e-mail desconhecido')
+        
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type ('lais.teste@teste.com.br')
+        cy.get('#password').type ('teste@126')
+        cy.get('.woocommerce-form > .button').click()
+    });
+
 } )
+
+ 
